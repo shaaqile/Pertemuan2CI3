eeb2655576f6abf9e3890df9c4e8d5539c9c2c2e
shaaqile <maqilak@gmail.com>
2022-04-21 13:56:31 +0700

    commit pertama

---
+++ application/views/home/landingPage.php
@@ -0,0 +1,36 @@
+<!--carousel-->
+
+<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner" style="height: 500px;">
+        <div class="carousel-item active">
+            <img src="<?= base_url('assets/'); ?>img/cat.jpg" class="d-block w-100" alt="..." style="margin-top: -100px;">
+        </div>
+        <div class="carousel-item">
+            <img src="<?= base_url('assets/'); ?>img/plant.jpg" class="d-block w-100" alt="..." style="margin-top: -200px;">
+        </div>
+        <div class="carousel-item">
+            <img src="<?= base_url('assets/'); ?>img/dog.jpg" class="d-block w-100" alt="..." style="margin-top: -300px;">
+        </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+</div>
+
+<!-- selamat datang -->
+
+<div class="selamatDatang">
+    <h1 style="text-align: center; margin-top: 30px;">Welcome To Pets and Plants friendly Website</h1>
+    <div class="row justify-content-center" style="margin-top: 30px; margin-bottom: 30px;">
+        <div class="col-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum molestias ipsa vero maxime consectetur veniam blanditiis neque dolore sapiente, eos deleniti nihil nulla! Maiores, facilis asperiores earum iste magnam natus. </div>
+        <div class="col-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid molestias minima sint pariatur vitae nam provident molestiae cupiditate nisi, dicta culpa repellat illum distinctio harum sit at quia asperiores accusantium.
+        </div>
+    </div>
+</div>
---
+++ application/views/pets/pet.php
@@ -0,0 +1,39 @@
+<div class="title">
+    <h1 style="margin-top: 30px; margin-bottom: 30px; margin-left: 100px;">Pets</h1>
+</div>
+
+
+<div class="container" style="margin-bottom: 30px;">
+    <div class="row justify-content-evenly">
+        <div class="col-4">
+            <div class="card" style="width: 18rem;">
+                <img src="<?= base_url('assets/'); ?>img/cat1.jpg" class="card-img-top" alt="..." style="height: 300px;">
+                <div class="card-body">
+                    <h5 class="card-title">Cat</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+                </div>
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="card" style="width: 18rem;">
+                <img src="<?= base_url('assets/'); ?>img/dog1.jpg" class="card-img-top" alt="..." style="height: 300px;">
+                <div class="card-body">
+                    <h5 class="card-title">Dog</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+                </div>
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="card" style="width: 18rem;">
+                <img src="<?= base_url('assets/'); ?>img/cow1.jpg" class="card-img-top" alt="..." style="height: 300px;">
+                <div class="card-body">
+                    <h5 class="card-title">Cow</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
