2d6cc1dd3ceb30e9d19d6fef8ce80b4f8b8dd7e5
shaaqile <maqilak@gmail.com>
2022-04-21 14:33:29 +0700

    commit final 2

--- application/views/home/landingPage.php
+++ application/views/home/landingPage.php
@@ -8,9 +8,6 @@
         <div class="carousel-item">
             <img src="<?= base_url('assets/'); ?>img/plant.jpg" class="d-block w-100" alt="..." style="margin-top: -200px;">
         </div>
-        <div class="carousel-item">
-            <img src="<?= base_url('assets/'); ?>img/dog.jpg" class="d-block w-100" alt="..." style="margin-top: -300px;">
-        </div>
     </div>
     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
